756545d7a81daa2fd960eefad4afadd8935362a4
RobertoNegro <RobertoNegro@users.noreply.github.com>
2023-07-28 11:25:53 +0000

    chore(format): code re-format (pint)

--- routes/nova-field-api.php
+++ routes/nova-field-api.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/ping', function (Request $request) {
     return response()->json([
-        'message' => 'pong'
+        'message' => 'pong',
     ]);
 })->name('ping');
